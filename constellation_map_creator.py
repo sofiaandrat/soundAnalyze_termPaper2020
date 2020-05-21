d8c0732b072189861d61d048598b54e1bc8314ac
sofiaandrat <sofiaanisimova@yandex.by>
2020-05-22 02:31:23 +0300

    'almost_star_map_is_ok'

--- constellation_map_creator.py
+++ constellation_map_creator.py
@@ -1,14 +1,41 @@
 from scipy.signal import find_peaks
 import pandas as pd
+import numpy as np
+
 
 class ConstellationMap:
     def __init__(self, data_frame):
         self.data_frame = data_frame
         self.stars_dict = {}
+        self.extremums = None
+        self.map = None
+        self.data_for_map_drawing = []
+        self.figsize = (0, 0)
+
+    def __str__(self):
+        string = ''
+        for raw in self.map.values:
+            string += " ".join(map(str, raw))
+            string += '\n'
+        return string
 
     def find_stars(self):
-        for column in self.data_frame.columns:
-            a = self.data_frame.[1][1]
-            peaks, _ = find_peaks(self.data_frame.loc[:, [column]][1])
-            print(self.data_frame.loc[:, [column]])
-            print(peaks)
+        self.extremums = self.__find_extrema__(self.data_frame)
+        self.__create_map__()
+
+    def __find_extrema__(self, tolerance=0.5):
+        diff = self.data_frame.diff()
+        extrema = diff[np.abs(diff) < tolerance]
+        return extrema
+
+    def __create_map__(self, tolerance=2):
+        self.map = self.extremums
+        self.map = self.map.fillna(0)
+        self.map = self.map.mask(abs(self.map) < tolerance, 0)
+        self.map = self.map.mask(abs(self.map) > tolerance, 1)
+        for time, row in self.map.iterrows():
+            for freq, element in np.ndenumerate(row):
+                if element > 0:
+                    self.data_for_map_drawing.append([freq[0], time])
+        self.data_for_map_drawing = np.array(self.data_for_map_drawing)
+        self.figsize = (self.map.values.shape[0] // 5, self.map.values.shape[1] // 10)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import wget
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 from converter import fromMp3ToWav
 from matrix_creator import VolumeMatrix
 from constellation_map_creator import ConstellationMap
@@ -10,26 +10,35 @@ from thinkdsp import read_wave, decorate  # no pep8
 
 if __name__ == "__main__":
 
-    src = 'D:/курсач/songs data/буквы/ё.mp3'
+    src = 'D:/курсач/songs data/Моё сердце остановилось/after/DaryaLoban.mp3'
     if 'mp3' in src:
         src = fromMp3ToWav(src)
     wave = read_wave(src)
     wave.normalize()
-    wave = wave.segment(start=0.5, duration=0.5)
+    #wave = wave.segment(start=0, duration=0.5)
     # pyplot.show()
     spectrum = wave.make_spectrum()
-    spectrum.plot()
-    decorate(xlabel='Frequency (Hz)', ylabel='Amplitude')
-    # pyplot.show()
+    #plt = spectrum.plot()
+    #decorate(xlabel='Frequency (Hz)', ylabel='Amplitude')
+    #pyplot.show()
     spectro = wave.make_spectrogram(seg_length=1024)
-    spectro.plot(high=4200)
-    decorate(xlabel='Time (s)',
-             ylabel='Frequency (Hz)')
+    #spectro.plot(high=4200)
+    #decorate(xlabel='Time (s)',
+    #         ylabel='Frequency (Hz)')
     # pyplot.show()
     matrix = VolumeMatrix(wave, (200, 2000), 0.01)
     matrix.decorator()
-    f = open("output.txt", "w+")
-    f.write(str(matrix))
-    f.close()
     const = ConstellationMap(matrix.table)
     const.find_stars()
+    const_map = const
+    stars = const_map.data_for_map_drawing
+    f = open("output.txt", "w+")
+    f.write(str(const_map))
+    f.close()
+    labels = range(1, len(stars) + 1)
+    plt.figure(figsize=const_map.figsize)
+    plt.subplots_adjust(bottom=0.1)
+    plt.scatter(stars[:, 0], stars[:, 1], label='True Position', marker="x")
+    plt.show()
+
+
